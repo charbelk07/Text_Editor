48342133cccd1419b9d086718be91955607465ce
charbelk07 <125899348+charbelk07@users.noreply.github.com>
2023-08-24 01:35:13 -0700

    install.js TODO completed

--- client/src/js/install.js
+++ client/src/js/install.js
@@ -1,11 +1,35 @@
-const butInstall = document.getElementById('buttonInstall');
+const butInstall = document.getElementById("buttonInstall");
 
 // Logic for installing the PWA
 // TODO: Add an event handler to the `beforeinstallprompt` event
-window.addEventListener('beforeinstallprompt', (event) => {});
+let defualtPrompt;
+
+window.addEventListener("beforeinstallprompt", (event) => {
+  event.preventDefault();
+  butInstall.style.visibility = "visible";
+  defPrompt = event;
+});
 
 // TODO: Implement a click event handler on the `butInstall` element
-butInstall.addEventListener('click', async () => {});
+butInstall.addEventListener("click", () => {
+  if (defualtPrompt) {
+    defualtPrompt.prompt();
+    defualtPrompt.userChoice.then((selectedResult) => {
+      if (selectedResult.outcome === "accepted") {
+        butInstall.setAttribute("disabled", true);
+        butInstall.textContent = "Successfully installed";
+      } else {
+        console.log("Why no install me?");
+      }
+    });
+  }
+
+  console.log("click");
+  console.error("there was an issue installing");
+});
 
 // TODO: Add an handler for the `appinstalled` event
-window.addEventListener('appinstalled', (event) => {});
+window.addEventListener("appinstalled", (event) => {
+  // Clear prompt
+  butInstall.textContent = "Application Installed, and its ready to be used!";
+});
